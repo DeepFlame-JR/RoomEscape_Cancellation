b597d9e65660e1690528de569e6d4eb8407a4699
DeepFlame-JR <wnsfuf0121@naver.com>
2022-03-07 23:26:22 +0900

    feat(user): MongoDB에 user 정보 저장

--- src/webserver/app.js
+++ src/webserver/app.js
@@ -1,12 +1,34 @@
 console.log('Server-side code running');
 
+// require packages
 const express = require("express");
-var path = require("path");
+const path = require("path");
+const sf = require("sf");
+const iniparser = require("iniparser");
+const bodyParser = require("body-parser")
+var mongoose = require("mongoose");
 
+// connect mongo
+var config = iniparser.parseSync(path.join(__dirname, '..', 'config.ini'));
+var uri = sf('mongodb://{0}:{1}@{2}:27017/roomdb?authSource=admin',
+              config.MONGO.user, config.MONGO.pw, config.MONGO.ip);
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
+const UserSchema = new mongoose.Schema({
+  email: String,
+  cafe: String,
+  theme: String,
+}, {collection:'user'});
+const User = mongoose.model("User", UserSchema);
+
+// declare app
 const app = express();
 const port = 3001;
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log("Listening on", port)
@@ -16,4 +38,9 @@ app.get('/', (req, res) => {
   res.sendFile('public/index.html');
 });
 
-// module.exports = app;
+app.post('/clicked', function(req, res){
+  var user = new User(req.body);
+  user.save()
+      .then(() => console.log("user information is added"))
+      .catch((err) => console.log(err));
+});
--- src/webserver/public/client.js
+++ src/webserver/public/client.js
@@ -1,14 +1,37 @@
-console.log('Client-side code running');
-
+// require packages
 const button = document.getElementById('registerButton');
 const email_element = document.getElementById('email');
 const theme_element = document.getElementById('theme');
 
+
 button.addEventListener('click', function(e) {
-  alert("test")
   var email = email_element.value;
-  var theme = theme_element.value;
-  var msg = "아래 정보가 맞습니까?\nE-mail: " + email + "\n테마: " + theme
-  alert(msg)
-  // console.log('button was clicked');
+  var select = theme_element.value;
+  var arr = select.split("|");
+  var cafe = arr[0].trim();
+  var theme = arr[1].trim();
+
+  var msg = "아래 정보가 맞습니까?\nE-mail: " + email + "\n카페: " + cafe + "\n테마: " + theme;
+  var result = confirm(msg);
+
+  if(result){
+    fetch('/clicked', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        cafe: cafe,
+        theme: theme,
+      })
+    })
+    .then(function(res){
+      if(res.ok){
+        alert('click was recorded');
+        return res.json();
+      }
+    })
+    .catch((err) => console.log(err));
+  }
 });
